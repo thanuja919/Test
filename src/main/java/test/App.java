c12e019b3346057d6be0b710ac36f4c86ed9bf04
thanuja919 <thanuja919@gmail.com>
2017-01-05 19:04:30 +0530

    Second commit

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Branch1");
+        System.out.println("Branch1 line 2");
+        System.out.println("Branch1 line 2");
     }
 }
